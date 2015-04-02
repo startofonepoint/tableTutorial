522b12ebafec1debd6835d1fad196521c204ac01
startofonepoint <startofonpoint@gmail.com>
2015-04-02 19:44:31 +0900

    First Commit

--- tableTutorial/ViewController.swift
+++ tableTutorial/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView:UITableView!
+    let mainData = ["one","two","three","four","five"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mainData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("thisCell") as UITableViewCell
+        
+        cell.textLabel?.text = mainData[indexPath.row]
+        return cell
+    }
 
 }
 
