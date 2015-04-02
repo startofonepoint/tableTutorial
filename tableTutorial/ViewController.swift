6cf7f2607e25f85087330a67688a42d60e348a24
startofonepoint <startofonpoint@gmail.com>
2015-04-02 15:59:32 +0900

    Initial Commit

---
+++ tableTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableTutorial
+//
+//  Created by lostin1 on 2015. 4. 2..
+//  Copyright (c) 2015년 lostin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
